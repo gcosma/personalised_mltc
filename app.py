428c8e28e56d4ee15520ef43e173ea047519c25f
Dr Georgina Cosma <g.cosma@lboro.ac.uk>
2024-12-28 08:36:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ def load_and_process_data(uploaded_file):
 @st.cache_data
 def perform_sensitivity_analysis(data, top_n=5):
     """Perform sensitivity analysis with configurable number of top trajectories"""
-    or_thresholds = [2.0, 3.0, 4.0, 5.0, 6.0, 7.0]
+    or_thresholds = [2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9, 10, 11, 12, 13, 14, 15]
     results = []
     total_patients = data['TotalPatientsInGroup'].iloc[0]
 
